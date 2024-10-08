a7a9a5790b31d45e067fcd156c16829afffe4a81
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-10-08 17:22:39 +0200

    cookiecutter

---
+++ js/widget.js
@@ -0,0 +1,17 @@
+import "./widget.css";
+
+function render({ model, el }) {
+	let btn = document.createElement("button");
+	btn.innerHTML = `count is ${model.get("value")}`;
+	btn.addEventListener("click", () => {
+		model.set("value", model.get("value") + 1);
+		model.save_changes();
+	});
+	model.on("change:value", () => {
+		btn.innerHTML = `count is ${model.get("value")}`;
+	});
+	el.classList.add("ipydrop");
+	el.appendChild(btn);
+}
+
+export default { render };
